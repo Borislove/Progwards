5ad84c7deb58dce7ea05c8ec52ee64dd9839edd0
Boris <abirme@yandex.ru>
2020-04-15 11:04:43 +0300

    t10 tests

--- src/ru/progwards/t10/i10/Exception2.java
+++ src/ru/progwards/t10/i10/Exception2.java
@@ -7,13 +7,15 @@ import java.io.IOException;
   IOException со строкой "File not found", в противном случает возвращает строку "File processing"*/
 public class Exception2 {
 
-    public String test(String filename) throws IOException {
-        try {
-            if (filename == null) ;
-            filename.toString();
-        } catch (NullPointerException e) {
-            throw new IOException("File not found");
-        }
+    public static String test(String filename) throws IOException {
+
+        if (filename == null)
+            throw new IOException();
+
         return "File processing";
+    }
+
+    public static void main(String[] args) throws IOException {
+        test(null);
     }
 }
--- src/ru/progwards/t10/i10/FileNotFound.java
+++ src/ru/progwards/t10/i10/FileNotFound.java
@@ -1,8 +1,36 @@
 package ru.progwards.t10.i10;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /*Реализовать метод с сигнатурой private int lineCount(String filename)
  который возвращает количество строк в файле filename.
   В случае ошибки пробросить исключение IOException со строкой сообщения "файл не найден"*/
 public class FileNotFound {
 
+
+    private static int lineCount(String filename) throws IOException {
+        try {
+            FileReader reader = new FileReader(filename);  //чтение файла
+            Scanner scanner = new Scanner(reader);
+            int count = 0;  //количество строк
+            try {
+                while (scanner.hasNextLine()) {
+                    String strFromFile = scanner.nextLine();
+                    count++;
+                }
+            } finally {
+                reader.close();
+                scanner.close();
+            }
+            return count;
+        } catch (Throwable e) {
+            throw new IOException("файл не найден");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(lineCount("logfile2.txt"));
+    }
 }
